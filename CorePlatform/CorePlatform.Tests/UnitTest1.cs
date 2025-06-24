0618293630645681f29b4e4c82eb66fb593a022a
Lucas Duarte <lucasduarte218@hotmail.com>
2025-06-24 17:31:03 -0300

    feat: camada de testes.

---
+++ CorePlatform/CorePlatform.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CorePlatform.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
